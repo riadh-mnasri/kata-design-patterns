856bc4ce65f3780e274e9678ebde8b5609d23247
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-08-28 14:17:26 +0200

    Add Command Pattern

---
+++ src/main/java/com/coding/dojo/designpatterns/command/Broker.java
@@ -0,0 +1,22 @@
+package com.coding.dojo.designpatterns.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Broker {
+
+    private List<Order> orderList = new ArrayList<>();
+
+    public void takeOrder(Order order) {
+        orderList.add(order);
+    }
+
+    public void placeOrders(){
+        for (Order order : orderList) {
+            order.execute();
+        }
+        orderList.clear();
+    }
+
+
+}
---
+++ src/main/java/com/coding/dojo/designpatterns/command/Order.java
@@ -0,0 +1,5 @@
+package com.coding.dojo.designpatterns.command;
+
+public interface Order {
+    void execute();
+}
